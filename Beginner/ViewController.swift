613b559765358d65ca566803d5dc79b081da3263
nguyendinhhung04 <dinhhung5a6@gmail.com>
2024-06-29 14:43:20 +0700

    First Commit

---
+++ Beginner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beginner
+//
+//  Created by macos on 29/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
